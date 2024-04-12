6a62df62181bf4b29e7d3e87e3fb281d0e6adc51
LucioZampella <162721696+LucioZampella@users.noreply.github.com>
2024-04-12 11:40:00 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,2 +1,18 @@
 def line():
-    print("TO DO")
+import math
+coef_a = float(input("Ingrese el coeficiente A: "))
+coef_b = float(input("Ingrese el coeficiente B: "))
+coef_X1 = float(input("Ingrese el coeficiente X1: "))
+coef_X2 = float(input("Ingrese el coeficiente X2 "))
+
+print(f"El coeficiente A de su ecuacion es: {coef_a}")
+print(f"El coeficiente B de su ecuacion es: {coef_b}")
+print(f"El coeficiente X1 de su ecuacion es: {coef_X1}")
+print(f"El coeficiente X2 de su ecuacion es: {coef_X2}")
+
+print(f"Para la siguiente ecuacion:\n{coef_a} + {coef_b}")
+
+print(f"Dado los siguientes puntos:\nP1 {coef_X1, coef_a*coef_X1 + coef_b} ")
+print(f"P1 {coef_X2, coef_a*coef_X2 + coef_b} ")
+
+print(f"La distancia entre ellos es:" , math.dist([coef_X1, coef_a*coef_X1 + coef_b] , [coef_X2, coef_a*coef_X2 + coef_b]))
